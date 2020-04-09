e4313511240ca54ab6d8942cede3c4e88b47d518
Artyom Lipovoy <oskarualds@mail.ru>
2020-04-09 18:02:34 +0300

    add code

---
+++ previos_and_next.py
@@ -0,0 +1,5 @@
+num = int(input ())
+pred = num - 1
+sled = num + 1
+print  ('The next number for the number ' , num , 'is' , sled)
+print  ('The previous number for the number ' , num , 'is' , pred)
