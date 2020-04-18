0c978cd2dadb3a578408ec533f018d206290d946
Artyom Lipovoy <oskarualds@mail.ru>
2020-04-18 18:40:44 +0300

    some another tasks

---
+++ decimal_notacion_of_a_number.py
@@ -0,0 +1,3 @@
+number=int(input())
+print ((number%100)//10)
+#developers: print(n // 10 % 10)
---
+++ sum_numbers.py
@@ -0,0 +1,3 @@
+number=int(input())
+print (number//100+(number%100)//10+number%10)
+#Дано трехзначное число. Найдите сумму его цифр. 
