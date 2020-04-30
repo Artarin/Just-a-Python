daa91ebcbcdee4db1867d95a45b6286c11e5d987
Artyom Lipovoy <oskarualds@mail.ru>
2020-04-30 17:00:43 +0300

    123

--- test_new_commands.py
+++ test_new_commands.py
@@ -1,2 +1,10 @@
-k=1
-n=0
+x='1+1=2'#input()
+l=str()
+print (x[:x.find('1')])
+print (x[x.find('1'):])
+
+for i in range(x.count('1')):
+    print (x[:x.find('1')])
+    l=(x[:x.find('1')])+'one'+(x[x.find('1')-1:])
+    print (l)   
+
