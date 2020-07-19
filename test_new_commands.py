3a2ac91b46eae2689ae71f16a40fd58c0c9c966d
Aryom Lipovoy <boss.lenovo@mail.ru>
2020-07-19 22:37:39 +0300

    test

--- test_new_commands.py
+++ test_new_commands.py
@@ -1,19 +1,8 @@
-num1 = int(input())
-num2 = int(input())
-i = 1
-k = 1
-while num2 != 0 :
-    
-    #if num2 == 0:
-     #   break
-    if num1 == num2:
-        i += 1  
-        print (i)          
+def power(a,n):
+    if n==0:
+        return 1
     else:
-        if k < i:
-            k = i
-        i = 1
-    num1, num2 = num2, num1
-    num2 = int(input())
- #   num1 = int(input())
-print (k)
+        return a*power(a,n-1) 
+a = float(input())
+n = int(input())
+print ((power(a,n)))
